7254cfc4af0a198c12d94220c9d1705e8b745185
Pasindu Yapa <lolmixage03@gmail.com>
2024-07-30 16:58:33 +0530

    Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,9 @@
 import Hero from "@/components/Hero";
-import Channels from "@/components/Channels";
-import TVShows from "@/components/TVShows";
-import Live from "@/components/Live";
-import Movies from "@/components/Movies";
-import MyList from "@/components/MyList";
 
 export default function Home() {
   return (
       <>
-          <Hero />
-          <Channels/>
-          <TVShows />
-          <Live />
-          <Movies />
-          <MyList />
+          <Hero/>
       </>
     );
 }
